e0de012500cc71a8b6ad3dd845608299c99a41f7
Ann-Cathrin Will <ann-cathrin.will@purple-agency.com>
2014-07-24 16:46:45 +0100

    basic a/b foundations set up

---
+++ a-b-test/a.php
@@ -0,0 +1,69 @@
+<?php include("../inc/head.php"); ?>
+<body>
+	<?php include("../inc/cookie.php"); include("../inc/header.php"); ?>
+
+	<div class="wrapper">
+		<div class="grid-wrapper">
+			<div class="grid">
+				<div class="grid__item one-whole">
+					<h1>What is A/B testing?</h1>
+					<p>Just like the name suggest it’s testing two different versions of an element against each other.</p> <p>Element A vs element B</p>
+					<p>For instance which cat do you prefer:</p>
+
+					Cat A (Lil bub)
+					Cat B (Tard)
+
+
+
+					<p>In terms of a website or email half the users would see Lil bub and the other half would see tard, you can then measure which one they liked more.</p>
+					<p>Let’s try it here, below you will either see a green button or a red button.</p>
+						<div class="text--center">
+							<a href="#" class="btn">Hmmmm shiny button</a>
+						</div>
+					<!-- <em>(link: red button users who clicked, green button users who clicked)
+						As you can see *** is currently more popular.</em> -->
+
+					<h2>Why do this?</h2>
+					<p>In short to improve the website or email. Maybe we think users like sliders everywhere and huge hero images on every page, and maybe they do! But the best way to find out is to test this on the users who will be using it.</p>
+
+					<p>With this we can:</p>
+					<ul>
+						<li>Design more effective emails/websites customised to the target audience</li>
+						<li>Improve click rates, page engagement and profit</li>
+						<li>Sell it as a service to our clients before or after a project has finished.</li>
+					</ul>
+
+					<p>Instead of doing little improvements that may do more damage than good we can actually test which one is more effective.</p>
+
+					<h2>So how can we sell this?</h2>
+					<h3>Emails</h3>
+					<p>A one of email is not the best choice, because there is nothing to action with the results once it’s sent out, but with an email that get’s sent out on a regular basis we can offer to test a complete redesign or maybe just a small part like the subject line, to see which one generates more clicks. Thereby improving the email for future use.</p>
+					<p>It’s important to note that changing the entire design will not give us insight into what part actually made the difference. So stick to simple small changes.</p>
+					<p>Don’t re-word the entire link and change the colour for instance. Focus on one thing at a time.</p>
+					<h3>Websites</h3>
+					<p>For websites there is a lot more than can be measured.</p>
+					<p>Including:</p>
+					<ul>
+						<li>how long the user spends on the page</li>
+						<li>amount of users that bounce(leave the website)</li>
+						<li>or custom events like sign up, click buttons, fill out forms.</li>
+					</ul>
+
+					<p>As with the emails changing an entire page can show us which one is better but will not tell us what in particular made the difference. Not to say that this should always be avoided. Just keep it in mind, a huge change won’t give us the specifics.</p>
+					<p>A few good changes include:</p>
+					<ul>
+						<li>heading wording</li>
+						<li>Call to action buttons</li>
+						<li>images (picture of a dog or cat)</li>
+						<li>hyperlink wording</li>
+					</ul>
+					<p>The great thing about websites is we can set up more extensive tests not just A against B but, 3 or 4 variations of an element. Every colour button if you like.</p>
+					<p>And this is something we can sell to any client, regardless of whether website was built by us or not,  finished long ago or only just gone live.</p>
+				</div>
+			</div><!-- /grid -->
+		</div><!-- /wrapper__inner -->
+	</div><!-- /wrapper -->
+
+	<?php include("../inc/footer.php"); include("../inc/scripts.php"); ?>
+</body>
+</html>
