693c60eddcbbf8b43eaae5d06cd3d3b09a1a7904
Ann-Cathrin <cathrin.will@googlemail.com>
2015-05-21 12:58:57 +0100

    grunt re config

--- gruntfile.js
+++ gruntfile.js
@@ -1,120 +1,96 @@
 module.exports = function(grunt) {
 
-	// Project configuration.
 	grunt.initConfig({
 		jshint: {
-			beforeconcat: ['assets/js/src/main.js'] // all we really want is for jshints to be turned on not to actually fail the file !
+			beforeconcat: ['src/js/main.js'] // all we really want is for jshints to be turned on not to actually fail the file !
 		},
-		uglify: {
+		min: {
 			files: {
-				src: [ 'assets/js/src/**/*.js' ],  // source files mask
-				dest: 'assets/js/min/main.min.js',    // destination folder
-				flatten: true,   // remove all unnecessary nesting
-				ext: '.min.js'   // replace .js to .min.js
+				src: [ 'src/js/plugins/*.js','src/js/main.js' ],
+				dest: 'dist/js/main.min.js',
+				flatten: true,
+				ext: '.min.js'
 			}
 		},
 		sass: {
 			options: {
-				style: 'compressed'
+				style: 'extended'
+				// style: 'compressed'
 				// compass: true
 			},
 			dist: {
 				files: {
-					'assets/css/style.css': 'assets/sass/style.scss'
+					'src/css/style.css': 'src/sass/style.scss'
 				},
 			},
 		},
-
+		tinypng: {
+			options: {
+				apiKey: 'fzeWdn5xkPkiOXTmhRDaZ9DLvKvp7wnn',
+				checkSigs: true,
+				sigFile: 'src/img/file_sigs.json',
+				summarize: true,
+				showProgress: true,
+				stopOnImageError: true
+			},
+			compress: {
+				expand: true,
+				cwd: 'src/img/',
+				src: ['**/*.jpg','**/*.png'],
+				dest: 'dist/img/'
+			}
+		},
+		autoprefixer: {
+			multiple_files: {
+				expand: true,
+				cwd: 'src/css/',
+				src: ['**/*.css'],
+				dest: 'dist/css/'
+			}
+		},
 		watch: {
 			scripts: {
-				files: ['assets/js/src/**/*.js'],
-				tasks: ['uglify'],
+				files: ['src/js/**/*.js'],
+				tasks: ['min'],
 				options: { nospawn: true }
 			},
 			lint: {
-				files: ['assets/js/src/main.js'], // don't need to lint plugins
+				files: ['src/js/main.js'], // don't need to lint plugins
 				tasks: ['jshint'],
 				options: { nospawn: true }
 			},
-			sass: {
-				files: ['assets/sass/**/*.scss'],
-				tasks: ['sass'],
-				options: {
-					nospawn: true,
-					livereload: true
-				}
+			css: {
+				files: ['src/sass/**/*.scss'],
+				tasks: ['sass', 'autoprefixer']
 			}
 		},
 		modernizr: {
 			dist: {
-				// [REQUIRED] Path to the build you're using for development.
-				"devFile" : "assets/js/vendor/modernizr-dev-2.8.2.js",
-
-				// [REQUIRED] Path to save out the built file.
-				"outputFile" : "assets/js/vendor/modernizr-custom.min.js",
-
-				// Based on default settings on http://modernizr.com/download/
-				"extra" : {
-					"shiv" : true,
-					"printshiv" : false,
-					"load" : true,
-					"mq" : false,
-					"cssclasses" : true
+				'devFile' : 'src/js/vendor/modernizr-dev-2.8.3.js',
+				'outputFile' : 'dist/js/vendor/modernizr-custom.min.js',
+				'extra' : {
+					'shiv' : true,
+					'printshiv' : false,
+					'load' : true,
+					'mq' : false,
+					'cssclasses' : true
 				},
-
-				// Based on default settings on http://modernizr.com/download/
-				"extensibility" : {
-					"addtest" : false,
-					"prefixed" : false,
-					"teststyles" : false,
-					"testprops" : false,
-					"testallprops" : false,
-					"hasevents" : false,
-					"prefixes" : false,
-					"domprefixes" : false
+				'files' : {
+					'src': ['src/css/**/*.css','src/js/main.js'] // these may have to be changed depending on plugin dependencies
 				},
-
-				// By default, source is uglified before saving
-				"uglify" : true,
-
-				// Define any tests you want to implicitly include.
-				"tests" : [],
-
-				// By default, this task will crawl your project for references to Modernizr tests.
-				// Set to false to disable.
-				"parseFiles" : true,
-
-				// When parseFiles = true, this task will crawl all *.js, *.css, *.scss files, except files that are in node_modules/.
-				// You can override this by defining a "files" array below.
-				"files" : {
-					"src": ['assets/css/**/*.css','assets/js/src/main.js'] // these may have to be changed depending on plugin dependencies 
-				},
-
-				// When parseFiles = true, matchCommunityTests = true will attempt to
-				// match user-contributed tests.
-				"matchCommunityTests" : false,
-
-				// Have custom Modernizr tests? Add paths to their location here.
-				"customTests" : []
+				'matchCommunityTests' : false,
 			}
 		}
 	});
-	// Default task(s).
-	grunt.registerTask('default', ['uglify','sass','jshint','modernizr']);
 
-	// Load the plugin that provides the "uglify" task.
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	// let's get sassy
+	grunt.registerTask('default', ['min','sass','jshint','modernizr']); // Default task(s)
+
+	grunt.loadNpmTasks('grunt-yui-compressor');
 	grunt.loadNpmTasks('grunt-contrib-sass');
-
-	//watch files change
 	grunt.loadNpmTasks('grunt-contrib-watch');
-
-	// jshint
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-
-	// modernizr
-	grunt.loadNpmTasks("grunt-modernizr");
-
+	grunt.loadNpmTasks('grunt-modernizr');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-tinypng');
 };
---
+++ src/js/main.js
@@ -0,0 +1,90 @@
+var main = (function() {
+	'use strict';
+
+/*------------------------------------*\
+	function go - nibble and pieces
+\*------------------------------------*/
+function go() {
+	$('.ac-nav').greenify({
+		color: 'orange',
+		backgroundColor: 'tomato',
+		anim: 'test'
+	}).addClass('because');
+	greenify.goAnim();
+
+	/*------------------------------------*\
+	GA event
+	\*------------------------------------*/
+	// $('a[data-ga]').on('click', function(e) {
+	// 	console.log('clicked');
+	// 	e.preventDefault();
+	// 	var link = $(this),
+	// 	gaData = link.data('ga');
+
+	// 	ga('send', 'event', {
+	// 		'eventCategory' : 'link',
+	// 		'eventAction' : 'click',
+	// 		'eventLabel' : gaData,
+	// 		'eventValue' : 1,
+	// 		'hitCallback' : function() {
+	// 			window.location.href = link.attr('href');
+	// 		}
+	// 	});
+	// });
+// var favicon=new Favico({
+//     animation:'slide'
+// });
+// favicon.badge(1);
+}
+
+/*------------------------------------*\
+Cookie Control
+\*------------------------------------*/
+function cookieControl() {
+	//  cookie bar
+	var cookie = $('#cookie');
+
+	$('#cookie-close').on('click',function () {
+		cookie.slideUp('fast');
+		$.cookie('peanut_butter_cookie', 'yes', {expires: 365, path: '/' });
+	});
+	var isSet  = $.cookie('peanut_butter_cookie');
+
+	if ( isSet == 'yes' ) {
+		cookie.hide();
+	}
+	else {
+		cookie.show();
+	}
+}
+/*------------------------------------*\
+Magnific pop up
+\*------------------------------------*/
+function magPop() {
+	$('.pop--img').magnificPopup({
+		type:'image',
+		zoom: {
+			enabled: true, // By default it's false, so don't forget to enable it
+			duration: 300, // duration of the effect, in milliseconds
+			easing: 'ease-in-out', // CSS transition easing function
+		}
+	});
+}
+
+
+/*------------------------------------*\
+Function control centre
+\*------------------------------------*/
+	var init = function() {
+		cookieControl();
+		go();
+		magPop();
+	};
+
+	return { init: init };
+
+})();
+
+$(function() {
+	main.init();
+});
