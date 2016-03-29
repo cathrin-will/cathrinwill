bcdc9c84b204add2c6f59a9d54e833946e8537dc
Ann-Cathrin Will <ann@cathrinwill.com>
2016-03-29 08:57:27 +0100

    voller birthday

---
+++ volker.php
@@ -0,0 +1,55 @@
+<?php include("inc/head.php"); ?>
+	<body class="wrapper--green">
+		<?php  //include("inc/header.php"); ?>
+
+		<div class="wrapper wrapper--green">
+			<div class="grid-wrapper">
+				<div class="grid">
+					<div class="grid__item one-whole text--center">
+						<div id='wrap'>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+							<div class='box'></div>
+
+						</div>
+
+						<h2 class="happy  flush--bottom">Happy</h2>
+						<h2 class="birthday">Birthday</h2>
+
+						<img src="dist/img/volker.jpg" alt="Volker" class="img-v">
+						<h1 class="volker">Volker</h1>
+					</div>
+				</div><!-- /grid -->
+			</div><!-- /grid-wrapper -->
+		</div><!-- /wrapper -->
+
+		<?php include("inc/footer.php"); include("inc/scripts.php"); ?>
+	</body>
+</html>
