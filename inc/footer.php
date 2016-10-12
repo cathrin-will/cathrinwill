b92e5812fe1577b4fac9d1416768d585bfa8d1be
Ann-Cathrin Will <ann@cathrinwill.com>
2016-10-12 22:12:55 +0100

    updated the year in the footer... bit late in the year

--- inc/footer.php
+++ inc/footer.php
@@ -3,7 +3,7 @@
 		<div class="grid-wrapper">
 			<div class="grid">
 				<div class="grid__item one-whole  lap-and-up-two-thirds">
-					 2015 Ann-Cathrin Will
+					 2016 Ann-Cathrin Will
 				</div><!--
 				--><div class="grid__item one-whole  lap-and-up-one-third footer__contact">
 						<a href="mailto:&#x61;&#x6E;&#x6E;&#x40;&#x63;&#x61;&#x74;&#x68;&#x72;&#x69;&#x6E;&#x77;&#x69;&#x6C;&#x6C;&#x2E;&#x63;&#x6F;&#x6D;">&#x61;&#x6E;&#x6E;&#x40;&#x63;&#x61;&#x74;&#x68;&#x72;&#x69;&#x6E;&#x77;&#x69;&#x6C;&#x6C;&#x2E;&#x63;&#x6F;&#x6D;</a>
