48f67c3851050c6b0a57533c8d0887c3af527a28
Ann-Cathrin Will <ann@cathrinwill.com>
2016-10-12 22:14:38 +0100

    swapped header nav around with icons

--- inc/header.php
+++ inc/header.php
@@ -2,15 +2,7 @@
 	<div class="wrapper  soft-half--ends">
 		<div class="grid-wrapper">
 			<div class="grid">
-				<div class="grid__item one-whole lap-and-up-four-tenths">
-					<div class="social">
-						<a href="https://plus.google.com/+AnnCathrinWill?rel=author" class="icon-googleplus"><span class="accessibility">Google+</span></a><!--
-						--><a href="https://github.com/cathrin-will" class="icon-github"><span class="accessibility">Github</span></a><!--
-						--><a href="https://twitter.com/cathrinwill" class="icon-twitter"><span class="accessibility">Twitter</span></a><!--
-						--><a href="https://www.linkedin.com/in/cathrinwill" class="icon-linkedin"><span class="accessibility">LinkedIn</span></a>
-					</div>
-				</div><!--
-				--><div class="grid__item one-whole lap-and-up-six-tenths text--right">
+				<div class="grid__item one-whole lap-and-up-six-tenths">
 					<nav class="nav--main">
 						<ul class="flush">
 							<li><a href="/" class="<?php echo ($_SERVER['PHP_SELF'] == '/index.php' ? ' active' : '');?>"><span class="icon-home"></span> Home</a></li><!--
@@ -20,6 +12,14 @@
 							<!-- <li><a href="/cookies.php">Cookies</a></li> -->
 						</ul>
 					</nav>
+				</div><!--
+				--><div class="grid__item one-whole lap-and-up-four-tenths  text--right">
+					<div class="social">
+						<a href="https://plus.google.com/+AnnCathrinWill?rel=author" class="icon-googleplus"><span class="accessibility">Google+</span></a><!--
+						--><a href="https://github.com/cathrin-will" class="icon-github"><span class="accessibility">Github</span></a><!--
+						--><a href="https://twitter.com/cathrinwill" class="icon-twitter"><span class="accessibility">Twitter</span></a><!--
+						--><a href="https://www.linkedin.com/in/cathrinwill" class="icon-linkedin"><span class="accessibility">LinkedIn</span></a>
+					</div>
 				</div>
 			</div><!-- /grid -->
 		</div><!-- /grid-wrapper -->
