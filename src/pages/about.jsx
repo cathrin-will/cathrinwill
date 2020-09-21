9c1040d63707d8e2d73dfd79b2cb489db53a6c31
Ann-Cathrin Will <acwill@rawnet.com>
2020-09-21 18:53:02 +0100

    change text

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -19,7 +19,7 @@ const IndexPage = () => {
         <Layout>
             <h1>{data.site.siteMetadata.title}</h1>
             <p>
-                I&#39;m an experienced Front-end Developer having worked at a variety of digital and marketing agencies for over 7 years. With strong skills in writing <strong>HTML</strong>,&nbsp;
+                I&#39;m an experienced Front-end Developer having worked at a variety of digital and marketing agencies for over 8 years. With strong skills in writing <strong>HTML</strong>,&nbsp;
                 <strong>CSS</strong> using <strong>Sass</strong> and <strong>javaScript</strong>.
             </p>
         </Layout>
