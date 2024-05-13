878edb2ffdd63b227823b922a5a4851b206ced24
AC Will <acwill@rawnet.com>
2024-05-13 17:27:13 +0100

    favicon + password

---
+++ middleware.js
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server'
+
+export const config = {
+    matcher: ['/', '/index'],
+}
+
+export function middleware() {
+    const basicAuth = req.headers.get('authorization')
+    const url = req.nextUrl
+
+    if (basicAuth) {
+        const authValue = basicAuth.split(' ')[1]
+        const [user, pwd] = atob(authValue).split(':')
+
+        if (user === '4dmin' && pwd === 'testpwd123') {
+            return NextResponse.next()
+        }
+    }
+    url.pathname = '/api/auth'
+
+    return NextResponse.rewrite(url)
+}
