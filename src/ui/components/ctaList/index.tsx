ebc5d8dbed4a7963b44b28b0ed83d61dc5efa264
AC Will <acwill@rawnet.com>
2024-07-25 17:33:12 +0100

    build error fix

--- src/ui/components/ctaList/index.tsx
+++ src/ui/components/ctaList/index.tsx
@@ -5,11 +5,16 @@ export default function CTAList({
     ctas,
     className,
 }: React.HTMLAttributes<HTMLParagraphElement> & {
-    ctas?: Sanity.CTA[]
+    ctas?: Sanity.Button[]
 }) {
     return (
         <nav className={cn(className, 'flex gap-4 items-center ')}>
-            {ctas?.map((cta, key) => <Button {...cta} key={key} />)}
+            {ctas?.map((cta, key) => (
+                <Button
+                    {...cta}
+                    key={key}
+                />
+            ))}
         </nav>
     )
 }
