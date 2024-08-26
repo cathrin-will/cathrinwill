4195c7aa527ec206f3fde9e0e11ce8fe9deaf8bb
AC Will <acwill@rawnet.com>
2024-08-26 20:57:27 +0100

    add colors to storybook

--- src/ui/components/text/index.stories.tsx
+++ src/ui/components/text/index.stories.tsx
@@ -16,12 +16,18 @@ export const Default: Story = {
     args: {
         children: (
             <>
-                <h2>Hello</h2>
+                <h1>heading 1</h1>
+                <h2>heading 2</h2>
+                <h3>heading 3</h3>
+                <h4>heading 4</h4>
+                <h5>heading 5</h5>
+                <h6>heading 6</h6>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Ducimus ullam beatae alias, cupiditate quo officia
-                    repellendus saepe id officiis reiciendis possimus libero
-                    esse amet excepturi at sint quidem aspernatur. Nesciunt.
+                    Ducimus ullam beatae alias, <a href='#'> link example</a>{' '}
+                    quo officia repellendus saepe id officiis reiciendis
+                    possimus libero esse amet excepturi at sint quidem
+                    aspernatur. Nesciunt.
                 </p>
                 <ul>
                     <li>lorem ipsum</li>
