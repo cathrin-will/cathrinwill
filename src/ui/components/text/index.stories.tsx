7ccf5b5efb68da4b452f110309ded6075e01f664
AC Will <acwill@rawnet.com>
2024-08-26 18:45:39 +0100

    text story + chromatic config

--- src/ui/components/text/index.stories.tsx
+++ src/ui/components/text/index.stories.tsx
@@ -14,6 +14,32 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
     args: {
-        children: <p>lorem ipsum</p>,
+        children: (
+            <>
+                <h2>Hello</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Ducimus ullam beatae alias, cupiditate quo officia
+                    repellendus saepe id officiis reiciendis possimus libero
+                    esse amet excepturi at sint quidem aspernatur. Nesciunt.
+                </p>
+                <ul>
+                    <li>lorem ipsum</li>
+                    <li>lorem ipsum</li>
+                    <li>lorem ipsum</li>
+                </ul>
+            </>
+        ),
+    },
+}
+
+export const SanityCMSContent: Story = {
+    args: {
+        content: [
+            {
+                _type: 'block',
+                children: [{ _key: '1', _type: 'span', text: 'lorem ipsum' }],
+            },
+        ],
     },
 }
