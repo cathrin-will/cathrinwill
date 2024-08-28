2b7a1636f1523e2fa83eba2f387bee6ae8fe2dbf
AC Will <acwill@rawnet.com>
2024-08-28 12:29:00 +0100

    jest setup

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -1,10 +1,10 @@
 import { createClient } from 'next-sanity'
-import isDev from '@/lib/env'
+import { isDev, apiVersion, projectId, dataset } from '@/lib/env'
 
 export default createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+    projectId,
+    dataset,
+    apiVersion,
     useCdn: !isDev,
     token: process.env.SANITY_API_TOKEN,
     stega: {
