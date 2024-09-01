bdec0bb6bb2bb67b4f9413bf06584d7e2660ae91
AC Will <acwill@rawnet.com>
2024-09-01 17:24:46 +0100

    chore: Refactor Lordicon component to improve code readability and performance

--- src/ui/components/lordicon/index.tsx
+++ src/ui/components/lordicon/index.tsx
@@ -3,20 +3,18 @@ import { Player } from '@lordicon/react'
 import { useRef } from 'react'
 import styles from './lordicon.module.scss'
 import { useEffect, useState } from 'react'
-export default function Lordicon({
-    icon = 'email',
-    link,
-    text,
-}: Model.Lordicon) {
+export default function Lordicon({ icon, link, text }: Model.Lordicon) {
     const playerRef = useRef<Player>(null)
     const [iconData, setIconData] = useState(null)
+    const loadIcon = async () => {
+        const data = await import(`../../icons/${icon}.json`)
+        setIconData(data)
+    }
 
     useEffect(() => {
-        const loadIcon = async () => {
-            const data = await import(`../../icons/${icon}.json`)
-            setIconData(data)
+        if (icon) {
+            loadIcon()
         }
-        loadIcon()
     }, [icon])
     const play = () => {
         playerRef.current?.playFromBeginning()
