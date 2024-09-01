849c1294186177f327246306fcfeed2a6ea79ef4
AC Will <acwill@rawnet.com>
2024-09-01 19:08:33 +0100

    block plop

--- plopfile.js
+++ plopfile.js
@@ -12,22 +12,55 @@ module.exports = function (plop) {
             {
                 type: 'add',
                 path: 'src/ui/components/{{camelCase name}}/index.tsx',
-                templateFile: 'plop-templates/Component.tsx.hbs',
+                templateFile: 'plop-templates/component/Component.tsx.hbs',
             },
             {
                 type: 'add',
                 path: 'src/ui/components/{{camelCase name}}/{{camelCase name}}.module.scss',
-                templateFile: 'plop-templates/Component.scss.hbs',
+                templateFile: 'plop-templates/component/Component.scss.hbs',
             },
             {
                 type: 'add',
                 path: 'src/ui/components/{{camelCase name}}/{{camelCase name}}.stories.tsx',
-                templateFile: 'plop-templates/Component.stories.hbs',
+                templateFile: 'plop-templates/component/Component.stories.hbs',
             },
             {
                 type: 'add',
                 path: 'src/ui/components/{{camelCase name}}/{{camelCase name}}.test.tsx',
-                templateFile: 'plop-templates/Component.test.hbs',
+                templateFile: 'plop-templates/component/Component.test.hbs',
+            },
+        ],
+    })
+
+    plop.setGenerator('block', {
+        description: 'Add a new block',
+        prompts: [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'block name:',
+            },
+        ],
+        actions: [
+            {
+                type: 'add',
+                path: 'src/ui/blocks/{{camelCase name}}Block/index.tsx',
+                templateFile: 'plop-templates/block/Block.tsx.hbs',
+            },
+            {
+                type: 'add',
+                path: 'src/ui/blocks/{{camelCase name}}Block/{{camelCase name}}.module.scss',
+                templateFile: 'plop-templates/block/Block.scss.hbs',
+            },
+            {
+                type: 'add',
+                path: 'src/ui/blocks/{{camelCase name}}Block/{{camelCase name}}.stories.tsx',
+                templateFile: 'plop-templates/block/Block.stories.hbs',
+            },
+            {
+                type: 'add',
+                path: 'src/ui/blocks/{{camelCase name}}Block/{{camelCase name}}.test.tsx',
+                templateFile: 'plop-templates/block/Block.test.hbs',
             },
         ],
     })
