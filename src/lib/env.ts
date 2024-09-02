39951720b3670ed30331620b9952ab6da98cee4d
AC Will <acwill@rawnet.com>
2024-09-02 21:00:59 +0100

    oopsie with the config 🙈

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,5 +4,5 @@ export const isDev =
 
 export const apiVersion =
     process.env.NEXT_PUBLIC_SANITY_API_VERSION ?? '2022-03-10'
-export const projectId = process.env.PROJECT_ID ?? '4z3s67le'
-export const dataset = process.env.DATASET ?? 'production'
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? '4z3s67le'
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'staging'
